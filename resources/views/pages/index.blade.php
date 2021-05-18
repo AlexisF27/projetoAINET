cccdcf3bbd60d2a9379598ed0a80b674064843b6
Alexis Fernando Rodriguez <alexisf_27@hotmail.com>
2021-05-18 12:23:42 +0100

    Definir layout/extend sections,Template Css (Boostrap)(tabs pendientes),PageController

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+
+@section('content')
+<h2>Apresentação</h2>
+<p>
+    O Departamento de Engenharia Informática (DEI) foi criado formalmente em 1997 com a aprovação
+    dos Estatutos da Escola Superior de Tecnologia e Gestão (ESTG), tendo no entanto as suas origens
+    na área de Informática existente na ESTG desde que esta entrou em funcionamento no ano letivo
+    1989/90.
+</p>
+
+@endsection
